7b3e29c372040bc4fdc3d8ecb418e6754e5a68d3
Devesh <deveshkumar21110@gmail.com>
2025-10-20 07:44:08 +0530

    feat: create UserAccountPage component and add initial structure

---
+++ frontend/src/pages/UserAccountPage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Container } from "../index";
+
+function UserAccountPage() {
+    
+
+  return (
+    <Container className="min-h-screen bg-pink-100 md:bg-white">
+      
+    </Container>
+  );
+}
+
+export default UserAccountPage;
